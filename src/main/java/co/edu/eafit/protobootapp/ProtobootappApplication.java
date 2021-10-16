90c1e49e73041c355e1e0eb76d65bb5cdf2c1cd9
zirei <zeatgomez@gmail.com>
2021-10-15 20:14:58 -0500

    I have created a comment

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//mi primer comentario.
+
 //App para hallar el cuadrado y el cubo de un número
 //Se lanza y se ejecuta con http://localhost:8080
 @SpringBootApplication
